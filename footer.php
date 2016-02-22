84dc5a324981bf26641057ccc9693a1f3006b2eb
chasingdonis <cuixianning@hotmail.com>
2016-02-21 18:05:17 -0600

    new theme - sanctuary

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package newtheme
+ */
+
+?>
+
+</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<h1>Contact Me</h1>
+			<p>Buddha House</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
